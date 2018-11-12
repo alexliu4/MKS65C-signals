1172e4cce9edb6c3afcecd8dfe1034199f0d27f7
alexliu4 <aliu9@stuy.edu>
2018-11-12 11:35:42 -0500

    finsihed assignment

--- signal.c
+++ signal.c
@@ -1,34 +1,24 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <unistd.h>
-#include <time.h>
-#include <pwd.h>
+#include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 static void sighandler(int signo){
 	if(signo==SIGSEGV){
-    printf("hello \n");
-		printf("What abt faulty programs\n");
+    printf("SIGINT intercepted \n");
+		exit(0);
   }
 	else if (signo==SIGUSR1){
-		printf("What you want from me?!?\n");
+		printf("parent pid: %d\n", getppid());
 	} //this function gives an error...ignore it(?)
 }
-
-//THE PUSH FINALLY WORKED   
-
 
 int main(){
 	signal(SIGUSR1, sighandler);
 	signal(SIGSEGV, sighandler);
 
 	while(1){
-		printf( "hello i am %s\n", getpid() );
+		printf( "hello i am %d\n", getpid() );
 		sleep(1);
 	}
 	return 0;
